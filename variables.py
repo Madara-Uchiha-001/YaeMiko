e55a9cf3f2f23aa94033b1003289ff38c7da3a73
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2024-07-29 05:51:28 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -10,31 +10,31 @@ class Config(object):
 
     # <================================================ REQUIRED ======================================================>
     # Telegram API configuration
-    API_ID = 6433468  # Get this value from my.telegram.org/apps
+    API_ID = ""  # Get this value from my.telegram.org/apps
     API_HASH = "7895dfd061f656367ccab30032"
 
     # Database configuration (PostgreSQL)
     DATABASE_URL = "postgres://ierjlkr:OG4dxzO67Zret3Zii43Hhvujkg89WVry0n9KsHE@karma.db.elephantsql.com/ierjlkr"
 
     # Event logs chat ID and message dump chat ID
-    EVENT_LOGS = -1001629811868
-    MESSAGE_DUMP = -1001629811868
+    EVENT_LOGS = -1002219564769
+    MESSAGE_DUMP = -1002227200116
 
     # MongoDB configuration
     MONGO_DB_URI = ""
 
     # Support chat and support ID
-    SUPPORT_CHAT = "ProjectCodeXSupport"
-    SUPPORT_ID = -1001629811868
+    SUPPORT_CHAT = "Ikigai Chats"
+    SUPPORT_ID = -1002029108799
 
     # Database name
-    DB_NAME = "MikoDB"
+    DB_NAME = ""
 
     # Bot token
-    TOKEN = "2323839365:AAFgfdadqawlfdsM7slOa33eM_ghop"  # Get bot token from @BotFather on Telegram
+    TOKEN = ""  # Get bot token from @BotFather on Telegram
 
     # Owner's Telegram user ID (Must be an integer)
-    OWNER_ID = 5907205317
+    OWNER_ID = 5745818770
     # <=======================================================================================================>
 
     # <================================================ OPTIONAL ======================================================>
@@ -44,7 +44,7 @@ class Config(object):
     BL_CHATS = []
 
     # User IDs of sudo users, dev users, support users, tiger users, and whitelist users
-    DRAGONS = []  # Sudo users
+    DRAGONS = [7144504368]  # Sudo users
     DEV_USERS = []  # Dev users
     DEMONS = []  # Support users
     TIGERS = []  # Tiger users
